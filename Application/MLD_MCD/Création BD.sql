c7b5089e812121c429f24a315c12fc999bd1bc30
Sacha Leone <Sacha.Leone@rpn.ch>
2024-12-04 12:06:51 +0100

    Initialized Laravel Herd project, with new migration tbl_vache and wampserver adress in .env

---
+++ Application/MLD_MCD/Création BD.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS bd_moocycle;
+CREATE DATABASE bd_moocycle;
+USE bd_moocycle:
+CREATE TABLE tbl_vache(
+   numero INT,
+   nom VARCHAR(50) NOT NULL,
+   date_prochaine_chaleur DATE,
+   date_insemination DATE,
+   date_naissance DATE NOT NULL,
+   nombre_lactation INT,
+   race VARCHAR(50) NOT NULL,
+   PRIMARY KEY(numero)
+);
---
+++ Application/Moocycle/database/migrations/2024_12_04_000000_create_tbl_vache.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tbl_vache', function (Blueprint $table) {
+            $table->integer('numero')->primary(); // Clé primaire
+            $table->string('nom', 50); // Champ VARCHAR(50) non nul
+            $table->date('date_prochaine_chaleur')->nullable(); // Champ DATE, nullable
+            $table->date('date_insemination')->nullable(); // Champ DATE, nullable
+            $table->date('date_naissance'); // Champ DATE non nul
+            $table->integer('nombre_lactation')->nullable(); // Champ INT, nullable
+            $table->string('race', 50); // Champ VARCHAR(50) non nul
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tbl_vache');
+    }
+};
